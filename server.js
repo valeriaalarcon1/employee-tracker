d498b1eb39e7aee167aa2ab45181e02d0b39cfaa
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-11-01 14:18:41 -0400

    Added code for prompt

--- server.js
+++ server.js
@@ -25,3 +25,26 @@ const db = mysql.createConnection(
 
 
 // FUNCTIONS
+
+function prompt() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update an Employee Role"
+            ]
+        })
+        .then(function ({action}) {
+            switch (action) {
+
+            }
+        })
+}
