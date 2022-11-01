1b961e66ff7b1700ead889de683ad4f3c5fb070a
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-11-01 14:03:54 -0400

    Added dependencies, middleware, and mysql connection code to server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require('express');
+// import and require MySQL2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'courses_db'
+  },
+  console.log(`Connected to the courses_db database.`)
+);
+
+
+// FUNCTIONS
